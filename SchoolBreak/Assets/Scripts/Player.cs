bdcc5fb26807e879835555b8a9f0fda7e1382b04
rafa <rafaelaaghellere@gmail.com>
2025-05-08 14:51:23 -0300

    PERGUNTAS FUNCIONAIS MDS

--- SchoolBreak/Assets/Scripts/Player.cs
+++ SchoolBreak/Assets/Scripts/Player.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -17,7 +18,12 @@ public class Player : MonoBehaviour
 
     //perguntas
     public Canvas question;
+    public TMP_Text questionText;
+    public TMP_Text[] optionTexts; 
+    public Button[] optionButtons;
     public bool isColliding = false;
+
+    private int correctAnswerIndex = -1;
 
     void Start()
     {
@@ -25,6 +31,11 @@ public class Player : MonoBehaviour
         anim = GetComponent<Animator>();
 
         question.gameObject.SetActive(false);
+
+        foreach (Button btn in optionButtons)
+        {
+            btn.onClick.RemoveAllListeners();
+        }
     }
 
     void Update()
@@ -79,11 +90,52 @@ public class Player : MonoBehaviour
     {
         if (obstacle.gameObject.CompareTag("Obstacle"))
         {
-            Debug.Log("tocou");
             question.gameObject.SetActive(true);
             isColliding = true;
             anim.SetInteger("transition", 0);
+
+            Questions info = obstacle.GetComponent<Questions>();
+            if (info != null)
+            {
+                var q = info.questionData;
+                questionText.text = q.question;
+                correctAnswerIndex = q.correctOptionIndex;
+
+                for (int i = 0; i < optionTexts.Length; i++)
+                {
+                    optionTexts[i].text = q.options[i];
+
+                    int index = i; // Captura o índice para o listener
+                    optionButtons[i].onClick.RemoveAllListeners();
+                    optionButtons[i].onClick.AddListener(() => OnOptionSelected(index));
+                }
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            isColliding = false;
+            question.gameObject.SetActive(false);
+        }
+    }
+
+    void OnOptionSelected(int selectedIndex)
+    {
+        if (selectedIndex == correctAnswerIndex)
+        {
+            Debug.Log("Resposta correta!");
+            // Você pode fazer algo como dar pontos, liberar caminho, etc.
+        }
+        else
+        {
+            Debug.Log("Resposta incorreta!");
+            // Penalidade, aviso, etc.
         }
 
+        // Fecha o canvas após resposta
+        question.gameObject.SetActive(false);
+        isColliding = false;
     }
 }
--- SchoolBreak/Assets/Scripts/Questions.cs
+++ SchoolBreak/Assets/Scripts/Questions.cs
@@ -1,6 +1,14 @@
 using UnityEngine;
 
-public class Questions
+[System.Serializable]
+public class QuestionData
 {
-    
+    [TextArea]
+    public string question;
+    public string[] options; 
+    public int correctOptionIndex; 
+}
+public class Questions : MonoBehaviour
+{
+    public QuestionData questionData;
 }
