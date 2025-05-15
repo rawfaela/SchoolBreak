f3fdadc6d1d255ced7947f6abb4b26f440082695
isa <isabelisantos0616@gmail.com>
2025-05-15 15:01:11 -0300

    TELA DE INICIO

---
+++ SchoolBreak/Assets/Scripts/ChamarJogoPrincipal.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChamarJogoPrincipal : MonoBehaviour
+{
+    public AudioSource musica;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        musica.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void MudarCena()
+    {
+        musica.Stop();
+        SceneManager.LoadScene(1);
+    }
+}
