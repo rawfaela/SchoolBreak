eb10adeff8eb5072a6df59af177888b991e761cf
rafa <rafaelaaghellere@gmail.com>
2025-05-26 14:45:29 -0300

    divisao scripts

--- SchoolBreak/Assets/Scripts/Player.cs
+++ SchoolBreak/Assets/Scripts/Player.cs
@@ -1,11 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -14,78 +8,41 @@ public class Player : MonoBehaviour
     public float Gravity = 10f;
     public float jumpForce = 3f;
 
-    Vector3 MoveDirection;
-    CharacterController controller;
-    Animator anim;
+    private Vector3 MoveDirection;
+    private CharacterController controller;
+    private Animator anim;
 
-    //perguntas
-    public Canvas question;
-    public TMP_Text questionText;
-    public TMP_Text[] optionTexts;
-    public Button[] optionButtons;
     public bool isCollidingObstacle = false;
-    private int correctAnswerIndex = -1;
-    private float questionTimer = 0f;
-    private bool questionActive = false;
-    private float extraTimeFromClocks = 0f;
-    public TMP_Text questionTimerText;
-    public TMP_Text ErrorsText;
-    public int contErrors = 0;
-    private Coroutine questionCoroutine; //tempo tava contando super rapido quando pegava o relogio
-    private Collider obstacleCollider; //p desativar o collider do obstaculo dps de acertar a pergunta
-
     public ChangeScenes changeScenes;
 
-    void Start()
+    private void Start()
     {
         controller = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
-
-        question.gameObject.SetActive(false);
-
-        foreach (Button btn in optionButtons)
-        {
-            btn.onClick.RemoveAllListeners();
-        }
-
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
-    void Update()
+    private void Update()
     {
         if (!isCollidingObstacle)
         {
             Move();
             Rotate();
         }
-
-        if (contErrors == 3)
-        {
-            changeScenes.SceneGameOver();
-        }
     }
 
-    void Move()
+    private void Move()
     {
         if (controller.isGrounded)
         {
             float horizontal = Input.GetAxis("Horizontal");
             float vertical = Input.GetAxis("Vertical");
             bool jump = Input.GetButtonDown("Jump");
-
             Vector3 move = transform.right * horizontal + transform.forward * vertical;
-
             MoveDirection = move * Speed;
 
-            if (vertical != 0 || horizontal != 0)
-            {
-                anim.SetInteger("transition", 1);
-            }
-            else
-            {
-                anim.SetInteger("transition", 0);
-            }
+            anim.SetInteger("transition", (vertical != 0 || horizontal != 0) ? 1 : 0);
             if (jump)
             {
                 anim.SetInteger("transition", 2);
@@ -99,7 +56,8 @@ public class Player : MonoBehaviour
 
         controller.Move(MoveDirection * Time.deltaTime);
     }
-    void Rotate()
+
+    private void Rotate()
     {
         Vector3 lookDirection = cameraTransform.forward;
         lookDirection.y = 0f;
@@ -108,152 +66,43 @@ public class Player : MonoBehaviour
         {
             Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10f * Time.deltaTime);
-        };
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        //perguntas
-        if (other.gameObject.CompareTag("Obstacle"))
+        if (other.CompareTag("Obstacle"))
         {
-            Questions info = other.GetComponent<Questions>();
-
-            // Se já respondeu corretamente, ignora a pergunta mas mantém o collider
-            if (info != null && info.alreadyAnsweredCorrectly)
-            {
-                return;
-            }
-
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-
-            questionTimer = 25f + extraTimeFromClocks;
-            extraTimeFromClocks = 0f; //reseta pq ja foi usado
-            questionActive = true;
-
-            if (obstacleCollider != null)
-            {
-                obstacleCollider.enabled = true; //ativa o collider do ultimo obstaculo (se tiver)
-            }
-            obstacleCollider = other; //salva o collider desse
-
-            if (questionCoroutine != null) //tempo tava contando super rapido quando pegava o relogio
-            {
-                StopCoroutine(questionCoroutine);
-            }
-            questionCoroutine = StartCoroutine(QuestionTime());
-
-            question.gameObject.SetActive(true);
-            isCollidingObstacle = true;
-            anim.SetInteger("transition", 0);
-
-            if (info != null)
-            {
-                var q = info.questionData;
-                questionText.text = q.question;
-                correctAnswerIndex = q.correctOptionIndex;
-
-                for (int i = 0; i < optionTexts.Length; i++)
-                {
-                    optionTexts[i].text = q.options[i];
-
-                    int index = i;
-                    optionButtons[i].onClick.RemoveAllListeners();
-                    optionButtons[i].onClick.AddListener(() => OnOptionSelected(index));
-                }
-            }
+            Questions question = other.GetComponent<Questions>();
+            question.ShowQuestion(this);
         }
 
-        if (other.gameObject.CompareTag("Boost"))
+        if (other.CompareTag("Clock"))
+        {
+            Questions question = other.GetComponent<Questions>();
+            if (question != null)
+                question.AddExtraTime(5f);
+        }
+
+        if (other.CompareTag("Boost"))
         {
             StartCoroutine(BoostSpeed(2.5f, 5));
         }
-
-        if (other.gameObject.CompareTag("Clock"))
-        {
-            extraTimeFromClocks += 5f;
-        }
     }
+
+
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Obstacle"))
         {
             isCollidingObstacle = false;
-            question.gameObject.SetActive(false);
         }
     }
 
-    //boost velocidade quando player pega tenis 
     private IEnumerator BoostSpeed(float multiplier, float duration)
     {
         Speed *= multiplier;
         yield return new WaitForSeconds(duration);
         Speed /= multiplier;
     }
-
-    //tempo da pergunta com e sem boost
-    private IEnumerator QuestionTime()
-    {
-        while (questionTimer > 0f)
-        {
-            questionTimer -= Time.deltaTime;
-            questionTimerText.text = $"{Mathf.CeilToInt(questionTimer)}s";
-            yield return null;
-        }
-
-        if (questionActive)
-        {
-            question.gameObject.SetActive(false);
-            isCollidingObstacle = false;
-            questionActive = false;
-        }
-        questionTimerText.text = "";
-    }
-
-    //perguntas
-    void OnOptionSelected(int selectedIndex)
-    {
-        if (selectedIndex == correctAnswerIndex)
-        {
-            Debug.Log("Resposta correta!");
-            optionButtons[selectedIndex].image.color = Color.green;
-            StartCoroutine(CloseQuestion(1f));
-            // Marca que já foi respondida corretamente
-            if (obstacleCollider != null)
-            {
-                Questions info = obstacleCollider.GetComponent<Questions>();
-                if (info != null)
-                {
-                    info.alreadyAnsweredCorrectly = true;
-                }
-            }
-        }
-        else
-        {
-            Debug.Log("Resposta incorreta!");
-            optionButtons[selectedIndex].image.color = Color.red;
-            StartCoroutine(CloseQuestion(1f));
-            ErrorsText.text = $"{ErrorsText.text}X";
-            contErrors += 1;  //QUANDO CHEGAR A 3 VOLTAR DETENÇÃO
-        }
-    }
-
-    IEnumerator CloseQuestion(float delay)
-    {
-        yield return new WaitForSeconds(delay);
-
-        //reseta a cor dos botões
-        foreach (var btn in optionButtons)
-        {
-            btn.image.color = Color.white;
-        }
-
-        question.gameObject.SetActive(false);
-        isCollidingObstacle = false;
-        questionActive = false;
-
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
-    //comentar: ctrl k ctrl c  descomentar: ctrl k ctrl u
 }
--- SchoolBreak/Assets/Scripts/Questions.cs
+++ SchoolBreak/Assets/Scripts/Questions.cs
@@ -1,15 +1,150 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class QuestionData
 {
-    [TextArea]
     public string question;
-    public string[] options; 
-    public int correctOptionIndex; 
+    public string[] options;
+    public int correctOptionIndex;
 }
+
 public class Questions : MonoBehaviour
 {
     public QuestionData questionData;
     public bool alreadyAnsweredCorrectly = false;
+
+    public Canvas questionCanvas;
+    public TMP_Text questionText;
+    public TMP_Text[] optionTexts;
+    public Button[] optionButtons;
+    public TMP_Text questionTimerText;
+    public TMP_Text errorsText;
+
+    private int correctAnswerIndex = -1;
+    private float questionTimer = 0f;
+    private bool questionActive = false;
+    private float extraTime = 0f;
+    private Coroutine questionCoroutine;
+    private Player playerRef;
+
+    public int contErrors = 0;
+
+    private void Start()
+    {
+        questionCanvas.gameObject.SetActive(false);
+    }
+
+    public void ShowQuestion(Player player)
+    {
+        if (alreadyAnsweredCorrectly)
+        {
+            return;
+        }
+
+        playerRef = player;
+        playerRef.isCollidingObstacle = true;
+        playerRef.GetComponent<Animator>().SetInteger("transition", 0);
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        questionCanvas.gameObject.SetActive(true);
+
+        questionTimer = 25f + extraTime;
+        extraTime = 0f;
+        questionActive = true;
+
+        if (questionCoroutine != null)
+            StopCoroutine(questionCoroutine);
+
+        questionCoroutine = StartCoroutine(QuestionTimer());
+
+        questionText.text = questionData.question;
+        correctAnswerIndex = questionData.correctOptionIndex;
+
+        for (int i = 0; i < optionTexts.Length; i++)
+        {
+            optionTexts[i].text = questionData.options[i];
+            int index = i;
+            optionButtons[i].onClick.RemoveAllListeners();
+            optionButtons[i].onClick.AddListener(() => OnOptionSelected(index));
+        }
+    }
+
+    public void AddExtraTime(float time)
+    {
+        extraTime += time;
+    }
+
+    private IEnumerator QuestionTimer()
+    {
+        while (questionTimer > 0f)
+        {
+            questionTimer -= Time.deltaTime;
+            questionTimerText.text = $"{Mathf.CeilToInt(questionTimer)}s";
+            yield return null;
+        }
+
+        if (questionActive)
+        {
+            contErrors++;
+            errorsText.text += "X";
+
+            CloseQuestion();
+            if (contErrors == 3)
+            {
+                playerRef.changeScenes.SceneGameOver();
+            }
+        }
+    }
+
+    private void OnOptionSelected(int index)
+    {
+        if (index == correctAnswerIndex)
+        {
+            optionButtons[index].image.color = Color.green;
+            alreadyAnsweredCorrectly = true;
+        }
+        else
+        {
+            optionButtons[index].image.color = Color.red;
+            contErrors++;
+            errorsText.text += "X";
+
+            if (contErrors == 3)
+            {
+                playerRef.changeScenes.SceneGameOver();
+            }
+        }
+
+        StartCoroutine(CloseQuestionDelayed(1f));
+    }
+
+    private IEnumerator CloseQuestionDelayed(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        CloseQuestion();
+    }
+
+    private void CloseQuestion()
+    {
+        foreach (var btn in optionButtons)
+        {
+            btn.image.color = Color.white;
+        }
+
+        questionCanvas.gameObject.SetActive(false);
+        questionTimerText.text = "";
+        questionActive = false;
+
+        if (playerRef != null)
+        {
+            playerRef.isCollidingObstacle = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
 }
