82f23a335722ce7d33dd8ea80880b3e73459b522
Jenifer Vieira <168489623+JeniferSilvaVieira@users.noreply.github.com>
2025-06-13 17:11:23 -0300

    coloquei os sons nos objetos

--- SchoolBreak/Assets/Scripts/Item.cs
+++ SchoolBreak/Assets/Scripts/Item.cs
@@ -5,6 +5,9 @@ public class Item : MonoBehaviour
     // Velocidade de rotação (em graus por segundo) nos eixos X, Y e Z
     public Vector3 rotationSpeed = new Vector3(0f, 100f, 0f);
 
+    // Som que será tocado ao pegar o item
+    public AudioClip pickupSound;
+
     void Update()
     {
         // Rotaciona o objeto com base na velocidade e no tempo entre os frames
@@ -15,7 +18,13 @@ public class Item : MonoBehaviour
     {
         if (player.CompareTag("Player"))
         {
-            // Aqui você pode adicionar pontuação, efeitos, som, etc.
+            // Toca o som na posição do item
+            if (pickupSound != null)
+            {
+                AudioSource.PlayClipAtPoint(pickupSound, transform.position);
+            }
+
+            // Aqui você pode adicionar pontuação, efeitos, etc.
             Destroy(gameObject); // Remove o item da cena
         }
     }
